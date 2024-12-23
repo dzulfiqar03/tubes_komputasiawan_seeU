bccfbe91d617a36e19454703fc12dc40f30e4fa4
Muhammad Dzulfiqar <muhammaddzulfiqar03@gmail.com>
2024-12-23 21:44:51 +0700

    new

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -4,12 +4,13 @@
 
 <nav class="navbar main-header mainColor mb-0">
     <div class="container-fluid">
-        
+
         <div class="d-flex">
-            
+
             <div class="btnTgl1" id="btnTgl1">
                 <li class="nav-item">
-                    <a class="nav-link text-white" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars m-auto"></i></a>
+                    <a class="nav-link text-white" data-widget="pushmenu" href="#" role="button"><i
+                            class="fas fa-bars m-auto"></i></a>
                 </li>
             </div>
 
@@ -21,69 +22,65 @@
             </div>
         </div>
 
-        <button class="navbar-toggler bg-warning text-dark p-2" type="button" data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-            <i class="bi-person-circle"></i>
-            Welcome,
 
-            @if (Auth::check())
-                {{ Auth::user()->userName }}!
-            @else
-                Guest!
-            @endif
-        </button>
-        <div class="offcanvas offcanvas-end text-bg-dark justify-center items-center" tabindex="-1" id="offcanvasDarkNavbar"
-            aria-labelledby="offcanvasDarkNavbarLabel">
-            <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">My Profile</h5>
-                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"
-                    aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body text-center">
-                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                    <li class="nav-item mb-5">
-                        
-                        @if (Auth::check())
-                            <img class="rounded-circle"
-                                src="{{ Storage::url('images/' . Auth::user()->original_filename) }}"
-                                width="200px" height="200px" alt="image">
-                        @else
-                            <img class="mx-auto mb-5" src="{{ Vite::asset('/resources/images/profile.png') }}"
-                                width="200px" height="200px" alt="image">
-                        @endif
 
-                    </li>
-                    <li class="nav-item">
+        <div
+            class="absolute pt-3 bottom-10 inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <button type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                <span class="absolute -inset-1.5"></span>
+                <span class="sr-only">View notifications</span>
+                <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+                </svg>
+              </button>
 
-                        @if (Auth::check())
-                            <h5 class="fw-bold">{{ Auth::user()->fullName }}</h5>
-                        @endif
-                    </li>
-                    <li class="nav-item">
-                        @if (Auth::check())
-                            <h5 class="fw-bold">{{ Auth::user()->email }}</h5>
-                        @endif
-                    </li>
+            <!-- Profile dropdown -->
+            <div class="relative ml-3">
+                <!-- Include Alpine.js -->
+                <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.15.0/dist/cdn.min.js" defer></script>
 
-                    <li class="nav-item mt-5">
-                        @if (Auth::check())
-                            <h5 class="fw-bold">{{ Auth::user()->role }}</h5>
-                        @endif
-                    </li>
+                <div x-data="{ open: false }" class="relative">
+                    <!-- User Button -->
+                    <button type="button"
+                        class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                        @click="open = !open" @keydown.escape.window="open = false" id="user-menu-button"
+                        aria-expanded="false" aria-haspopup="true">
+                        <span class="sr-only">Open user menu</span>
+                        <img width="50" height="100" class="rounded-full"
+                        src="{{ Storage::url('images/' . Auth::user()->original_filename) }}" 
+                            alt="User Avatar">
+                    </button>
 
-                </ul>
+                    <!-- Dropdown Menu -->
+                    <div x-show="open" @click.away="open = false" x-transition:enter="transition ease-out duration-100"
+                        x-transition:enter-start="transform opacity-0 scale-95"
+                        x-transition:enter-end="transform opacity-100 scale-100"
+                        x-transition:leave="transition ease-in duration-75"
+                        x-transition:leave-start="transform opacity-100 scale-100"
+                        x-transition:leave-end="transform opacity-0 scale-95"
+                        class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none"
+                        role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button">
+                        <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                            role="menuitem" data-widget="pushmenu">Your Profile</a>
+            
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <button class="btn btn-danger w-100 fw-bold btnReg border-0"
+                                    type="submit">Logout</button>
+                            </form>
+
+                    </div>
+                </div>
 
             </div>
-        </div>
-    </div>
 </nav>
 
 <style>
-    .offcanvas-backdrop.show{
+    .offcanvas-backdrop.show {
         opacity: 0;
     }
 
-    .rightContent{
+    .rightContent {
         padding: 0;
     }
 </style>
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,14 +1,72 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar mainColor h-full">
 
-        <div class="all-sidebar mainColor h-screen px-3">
+        <div class="all-sidebar mainColor h-screen px-1 mt-4">
             <!-- Brand Logo -->
             <a href="index3.html" class="brand-link ">
-                <img class="mx-auto mb-5" src="{{ Vite::asset('resources/images/Logo/logo_verti.png') }}" width="200px"
+                <img class="mx-auto mb-2" src="{{ Vite::asset('resources/images/Logo/logo_verti.png') }}" width="200px"
                     alt="image">
 
             </a>
 
+            <div id="accordion-collapse" class="px-3 pb-5" data-accordion="collapse">
+
+                <h2 id="accordion-collapse-heading-1">
+                    <button type="button"
+                    style="background-color: #3b4469"
+                        class="flex items-center w-full justify-between p-2 font-medium rtl:text-right text-gray-500  rounded-pill focus:ring-4 click: focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
+                        data-accordion-target="#accordion-collapse-body-1" aria-expanded="false"
+                        aria-controls="accordion-collapse-body-1">
+
+                        <div class="row">
+                            <div class="col">
+                                @if (Auth::check())
+                                    <!-- Profile dropdown -->
+                                    <div class="relative ml-3">
+                                        <div>
+                                            <img class="size-2 rounded-circle object-cover img-responsive"
+                                                src="{{ Storage::url('images/' . Auth::user()->original_filename) }}" 
+                                                alt="">
+                                        </div>
+                                    </div>
+                                    
+                                    @else
+                                        <img class="mx-auto mb-5"
+                                            src="{{ Vite::asset('/resources/images/profile.png') }}" width="40px"
+                                            height="40px" alt="image">
+                                @endif
+                            </div>
+
+                            <div class="col">
+                                @if (Auth::check())
+                                    <h5 class="fw-bold text-center text-white">{{ Auth::user()->fullName }}</h5>
+                                @endif
+                            </div>
+                        </div>
+
+
+                        <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M9 5 5 1 1 5" />
+                        </svg>
+                    </button>
+                </h2>
+               <!-- Accordion Content -->
+               <div id="accordion-collapse-body-1" class="hidden" aria-labelledby="accordion-collapse-heading-1">
+                <div class="p-4 bg-gray-800 text-center text-white rounded-lg dark:border-gray-700 dark:bg-gray-900">
+                    @if (Auth::check())
+                    <h5 class="mb-2">{{ Auth::user()->email }}</h5>
+                    <hr class="border-gray-600 my-2">
+                    <h6 class="mb-0">{{ Auth::user()->role }}</h6>
+                    @else
+                    <h5 class="mb-0">Please log in to access your profile information</h5>
+                    @endif
+                </div>
+            </div>
+
+            </div>
+            <hr>
             <!-- Sidebar -->
             <div class="sidebar">
 
@@ -16,89 +74,75 @@
                 <nav class="mt-2">
                     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
 
-                        <div id="accordion-collapse" class="mt-5 pt-5" data-accordion="collapse">
-
-                            <div class="w-100 mt-5 h-100">
-                                <div class="d-grid">
-                                    @if ($currentRouteName == 'home')
-                                        <a class="btn btn-warning fw-bold mb-3"
-                                            href="{{ route('home', ['id' => Auth::user()->id]) }}">Home</a>
-
-                                        <a class="btn btn-dark mb-3" style="color:rgb(70, 70, 70)"
-                                            href="{{ route('allUmkm') }}">UMKM</a>
-                                        <a class="btn btn-dark mb-3" style="color:rgb(70, 70, 70)"
-                                            href="{{ route('about') }}">About
-                                            Us</a>
-                                    @elseif($currentRouteName == 'allUmkm')
-                                        <a class="btn btn-dark  mb-3" style="color:rgb(70, 70, 70)"
-                                            href="{{ route('home', ['id' => Auth::user()->id]) }}">Home</a>
-
-                                        <a class="btn btn-warning fw-bold   mb-3" href="{{ route('allUmkm') }}">UMKM</a>
-                                        <a class="btn btn-dark mb-3" style="color:rgb(70, 70, 70)"
-                                            href="{{ route('about') }}">About
-                                            Us</a>
-                                    @elseif($currentRouteName == 'about')
-                                        <a class="btn btn-dark  mb-3" style="color:rgb(70, 70, 70)"
-                                            href="{{ route('home', ['id' => Auth::user()->id]) }}">Home</a>
-
-                                        <a class="btn btn-dark  mb-3" style="color:rgb(70, 70, 70)"
-                                            href="{{ route('allUmkm') }}">UMKM</a>
-                                        <a class="btn btn-warning fw-bold  mb-3" href="{{ route('about') }}">About
-                                            Us</a>
-                                    @elseif(Auth::user()->role == 'admin' && $currentRouteName == 'admins')
-                                        <a class="btn btn-warning  fw-bold mb-3"
-                                            href="{{ route('admin.index') }}">Home</a>
-
-                                        <a class="btn btn-dark  mb-3" href="{{ route('dataUmkm') }}"
-                                            style="color:rgb(70, 70, 70)">UMKM</a>
-                                        <a class="btn btn-dark  mb-3" href="{{ route('dataUser') }}"
-                                            style="color:rgb(70, 70, 70)">Users</a>
-                                       
-                                    @elseif(Auth::user()->role == 'admin' && $currentRouteName == 'dataUser')
-                                        <a class="btn btn-dark mb-3" href="{{ route('admin.index') }}"
-                                            style="color:rgb(70, 70, 70)">Home</a>
-
-                                        <a class="btn btn-dark mb-3" href="{{ route('dataUmkm') }}"
-                                            style="color:rgb(70, 70, 70)">UMKM</a>
-                                        <a class="btn btn-warning fw-bold  mb-3"
-                                            href="{{ route('dataUser') }}">Users</a>
-                                      
-                                    @elseif(Auth::user()->role == 'admin' && $currentRouteName == 'dataUmkm')
-                                        <a class="btn btn-dark mb-3" href="{{ route('admin.index') }}"
-                                            style="color:rgb(70, 70, 70)">Home</a>
-
-                                        <a class="btn btn-warning fw-bold  mb-3"
-                                            href="{{ route('dataUmkm') }}">UMKM</a>
-
-                                        <a class="btn btn-dark fw-bold  mb-3" href="{{ route('dataUser') }}"
-                                            style="color:rgb(70, 70, 70)">Users</a>
-
-                                     
-                                    @else
-                                        <a class="btn btn-dark  mb-3" style="color:rgb(70, 70, 70)"
-                                            href="{{ route('home', ['id' => Auth::user()->id]) }}">Home</a>
-
-                                        <a class="btn btn-dark  mb-3" style="color:rgb(70, 70, 70)"
-                                            href="{{ route('allUmkm') }}">UMKM</a>
-                                        <a class="btn btn-dark btn-dark mb-3" style="color:rgb(70, 70, 70)"
-                                            href="{{ route('about') }}">About
-                                            Us</a>
-                                    @endif
 
 
-                                </div>
+                        <div class="w-100 mt-5 h-100 p=5">
+                            <div class="d-grid">
+                                @if ($currentRouteName == 'home')
+                                    <a class="btn btn-warning fw-bold mb-3"
+                                        href="{{ route('home', ['id' => Auth::user()->id]) }}">Home</a>
+
+                                    <a class="btn btn-dark mb-3" style="color:rgb(70, 70, 70)"
+                                        href="{{ route('allUmkm') }}">UMKM</a>
+                                    <a class="btn btn-dark mb-3" style="color:rgb(70, 70, 70)"
+                                        href="{{ route('about') }}">About
+                                        Us</a>
+                                @elseif($currentRouteName == 'allUmkm')
+                                    <a class="btn btn-dark  mb-3" style="color:rgb(70, 70, 70)"
+                                        href="{{ route('home', ['id' => Auth::user()->id]) }}">Home</a>
+
+                                    <a class="btn btn-warning fw-bold   mb-3" href="{{ route('allUmkm') }}">UMKM</a>
+                                    <a class="btn btn-dark mb-3" style="color:rgb(70, 70, 70)"
+                                        href="{{ route('about') }}">About
+                                        Us</a>
+                                @elseif($currentRouteName == 'about')
+                                    <a class="btn btn-dark  mb-3" style="color:rgb(70, 70, 70)"
+                                        href="{{ route('home', ['id' => Auth::user()->id]) }}">Home</a>
+
+                                    <a class="btn btn-dark  mb-3" style="color:rgb(70, 70, 70)"
+                                        href="{{ route('allUmkm') }}">UMKM</a>
+                                    <a class="btn btn-warning fw-bold  mb-3" href="{{ route('about') }}">About
+                                        Us</a>
+                                @elseif(Auth::user()->role == 'admin' && $currentRouteName == 'admins')
+                                    <a class="btn btn-warning  fw-bold mb-3" href="{{ route('admin.index') }}">Home</a>
+
+                                    <a class="btn btn-dark  mb-3" href="{{ route('dataUmkm') }}"
+                                        style="color:rgb(70, 70, 70)">UMKM</a>
+                                    <a class="btn btn-dark  mb-3" href="{{ route('dataUser') }}"
+                                        style="color:rgb(70, 70, 70)">Users</a>
+                                @elseif(Auth::user()->role == 'admin' && $currentRouteName == 'dataUser')
+                                    <a class="btn btn-dark mb-3" href="{{ route('admin.index') }}"
+                                        style="color:rgb(70, 70, 70)">Home</a>
+
+                                    <a class="btn btn-dark mb-3" href="{{ route('dataUmkm') }}"
+                                        style="color:rgb(70, 70, 70)">UMKM</a>
+                                    <a class="btn btn-warning fw-bold  mb-3" href="{{ route('dataUser') }}">Users</a>
+                                @elseif(Auth::user()->role == 'admin' && $currentRouteName == 'dataUmkm')
+                                    <a class="btn btn-dark mb-3" href="{{ route('admin.index') }}"
+                                        style="color:rgb(70, 70, 70)">Home</a>
+
+                                    <a class="btn btn-warning fw-bold  mb-3" href="{{ route('dataUmkm') }}">UMKM</a>
+
+                                    <a class="btn btn-dark fw-bold  mb-3" href="{{ route('dataUser') }}"
+                                        style="color:rgb(70, 70, 70)">Users</a>
+                                @else
+                                    <a class="btn btn-dark  mb-3" style="color:rgb(70, 70, 70)"
+                                        href="{{ route('home', ['id' => Auth::user()->id]) }}">Home</a>
+
+                                    <a class="btn btn-dark  mb-3" style="color:rgb(70, 70, 70)"
+                                        href="{{ route('allUmkm') }}">UMKM</a>
+                                    <a class="btn btn-dark btn-dark mb-3" style="color:rgb(70, 70, 70)"
+                                        href="{{ route('about') }}">About
+                                        Us</a>
+                                @endif
 
 
                             </div>
 
-                            <div class="bottomContent">
-                                <form method="POST" action="{{ route('logout') }}">
-                                    @csrf
-                                    <button class="btn btn-danger w-100 fw-bold btnReg border-0"
-                                        type="submit">Logout</button>
-                                </form>
-                            </div>
+
                         </div>
+
+                    
 
 
                     </ul>
@@ -116,16 +160,5 @@
     </aside>
 
     <style>
-        .sideCont {
-            background-color: #90eb9639
-        }
-
-        .bodyCont {
-            background-image: linear-gradient(to bottom right, #48A84E, #4AD991);
-        }
-
-
-        .txt {
-            color: #48A84E;
-        }
+       
     </style>
